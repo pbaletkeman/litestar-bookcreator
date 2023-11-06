4d7ca488c0bb55bc693a99ec2f2ab97d1027f907
Pete Letkeman <pete@letkeman.ca>
2023-11-06 18:59:22 -0500

    refactored to shared file

--- meta_data.py
+++ meta_data.py
@@ -23,102 +23,13 @@ from litestar.contrib.sqlalchemy.repository import (
     SQLAlchemyAsyncRepository,
 )
 from litestar.di import Provide
-from litestar.openapi import OpenAPIConfig
 from litestar.params import Parameter
 from litestar.repository.filters import LimitOffset
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
-from shared import BaseModel, SlugKey, SQLAlchemyAsyncSlugRepository, provide_limit_offset_pagination
-
-# def provide_limit_offset_pagination(
-#     current_page: int = Parameter(ge=1, query="currentPage", default=1, required=False),
-#     page_size: int = Parameter(
-#         query="pageSize",
-#         ge=1,
-#         default=10,
-#         required=False,
-#     ),
-# ) -> LimitOffset:
-#     """Add offset/limit pagination.
-#
-#     Return type consumed by `Repository.apply_limit_offset_pagination()`.
-#
-#     Parameters
-#     ----------
-#     current_page : int
-#         LIMIT to apply to select.
-#     page_size : int
-#         OFFSET to apply to select.
-#     """
-#     return LimitOffset(page_size, page_size * (current_page - 1))
-
-#
-# class BaseModel(_BaseModel):
-#     """Extend Pydantic's BaseModel to enable ORM mode"""
-#
-#     model_config = {"from_attributes": True}
-
-
-# we are going to add a simple "slug" to our model that is a URL safe surrogate key to
-# our database record.
-# @declarative_mixin
-# class SlugKey:
-#     """Slug unique Field Model Mixin."""
-#
-#     __abstract__ = True
-#     slug: Mapped[str] = mapped_column(String(length=100), nullable=False, unique=True, sort_order=-9)
-#
-
-# this class can be re-used with any model that has the `SlugKey` Mixin
-# class SQLAlchemyAsyncSlugRepository(SQLAlchemyAsyncRepository[ModelT]):
-#     """Extends the repository to include slug model features."""
-#
-#     async def get_available_slug(self, value_to_slugify: str, **kwargs: Any, ) -> str:
-#         """Get a unique slug for the supplied value.
-#
-#         If the value is found to exist, a random 4 digit character is appended to the end.
-#         There may be a better way to do this, but I wanted to limit the number of
-#         additional database calls.
-#
-#         Args:
-#             value_to_slugify (str): A string that should be converted to a unique slug.
-#             **kwargs: stuff
-#
-#         Returns:
-#             str: a unique slug for the supplied value. This is safe for URLs and other
-#             unique identifiers.
-#         """
-#         slug = self._slugify(value_to_slugify)
-#         if await self._is_slug_unique(slug):
-#             return slug
-#         # generate a random 4 digit alphanumeric string to make the slug unique and
-#         # avoid another DB lookup.
-#         random_string = "".join(random.choices(string.ascii_lowercase + string.digits, k=4))
-#         return f"{slug}-{random_string}"
-#
-#     @staticmethod
-#     def _slugify(value: str) -> str:
-#         """slugify.
-#
-#         Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
-#         dashes to single dashes. Remove characters that aren't alphanumerics,
-#         underscores, or hyphens. Convert to lowercase. Also strip leading and
-#         trailing whitespace, dashes, and underscores.
-#
-#         Args:
-#             value (str): the string to slugify
-#
-#         Returns:
-#             str: a slugified string of the value parameter
-#         """
-#         value = unicodedata.normalize("NFKD", value).encode("ascii", "ignore").decode("ascii")
-#         value = re.sub(r"[^\w\s-]", "", value.lower())
-#         return re.sub(r"[-\s]+", "-", value).strip("-_")
-#
-#     async def _is_slug_unique(self, slug: str, **kwargs: Any, ) -> bool:
-#         return await self.get_one_or_none(slug=slug) is None
+from shared import SlugKey, SQLAlchemyAsyncSlugRepository, BaseModel
 
 
 # The `AuditBase` class includes the same UUID` based primary key (`id`) and 2
@@ -152,25 +63,60 @@ class MetaDataUpdate(BaseModel):
     description: str
 
 
+async def get_available_slug(self, value_to_slugify: str, **kwargs: Any, ) -> str:
+    """Get a unique slug for the supplied value.
+
+    If the value is found to exist, a random 4 digit character is appended to the end.
+    There may be a better way to do this, but I wanted to limit the number of
+    additional database calls.
+
+    Args:
+        value_to_slugify (str): A string that should be converted to a unique slug.
+        **kwargs: stuff
+
+    Returns:
+        str: a unique slug for the supplied value. This is safe for URLs and other
+        unique identifiers.
+    """
+    slug = self._slugify(value_to_slugify)
+    if await self._is_slug_unique(slug):
+        return slug
+    # generate a random 4 digit alphanumeric string to make the slug unique and
+    # avoid another DB lookup.
+    random_string = "".join(random.choices(string.ascii_lowercase + string.digits, k=4))
+    return f"{slug}-{random_string}"
+
+
+@staticmethod
+def _slugify(value: str) -> str:
+    """slugify.
+
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+
+    Args:
+        value (str): the string to slugify
+
+    Returns:
+        str: a slugified string of the value parameter
+    """
+    value = unicodedata.normalize("NFKD", value).encode("ascii", "ignore").decode("ascii")
+    value = re.sub(r"[^\w\s-]", "", value.lower())
+    return re.sub(r"[-\s]+", "-", value).strip("-_")
+
+
+async def _is_slug_unique(self, slug: str, **kwargs: Any, ) -> bool:
+    return await self.get_one_or_none(slug=slug) is None
+
+
 # we can optionally override the default `select` used for the repository to pass in
 # specific SQL options such as join details
 async def provide_meta_data_repo(db_session: AsyncSession) -> MetaDataRepository:
     """This provides a simple example demonstrating how to override the join options
     for the repository."""
     return MetaDataRepository(session=db_session)
-
-
-session_config = AsyncSessionConfig(expire_on_commit=False)
-sqlalchemy_config = SQLAlchemyAsyncConfig(
-    connection_string="sqlite+aiosqlite:///test.sqlite", session_config=session_config
-)  # Create 'async_session' dependency.
-sqlalchemy_plugin = SQLAlchemyInitPlugin(config=sqlalchemy_config)
-
-
-async def on_startup() -> None:
-    """Initializes the database."""
-    async with sqlalchemy_config.get_engine().begin() as conn:
-        await conn.run_sync(UUIDAuditBase.metadata.create_all)
 
 
 class MetaDataController(Controller):
@@ -233,20 +179,3 @@ class MetaDataController(Controller):
         """Delete a meta_data from the system."""
         _ = await meta_data_repo.delete(id)
         await meta_data_repo.session.commit()
-
-
-app = Litestar(
-    route_handlers=[MetaDataController],
-    openapi_config=OpenAPIConfig(
-        title="My API", version="1.0.0",
-        root_schema_site="elements",  # swagger, elements, redoc
-        path="/docs",
-        create_examples=False,
-    ),
-    exception_handlers={
-        # exceptions.ApplicationError: exceptions.exception_to_http_response,
-    },
-    on_startup=[on_startup],
-    plugins=[SQLAlchemyInitPlugin(config=sqlalchemy_config)],
-    dependencies={"limit_offset": Provide(provide_limit_offset_pagination, sync_to_thread=False)},
-)
---
+++ shared.py
@@ -0,0 +1,120 @@
+from __future__ import annotations
+
+import random
+import re
+import string
+import unicodedata
+from typing import TYPE_CHECKING, Any
+from uuid import UUID
+
+from litestar import HttpMethod
+from litestar import route
+from litestar.pagination import OffsetPagination
+from pydantic import BaseModel as _BaseModel
+from pydantic import TypeAdapter
+from sqlalchemy.orm import Mapped, declarative_mixin, mapped_column
+from sqlalchemy.types import String
+from litestar import Controller
+from litestar import Litestar, get, post, delete
+from litestar.contrib.sqlalchemy.base import UUIDAuditBase
+from litestar.contrib.sqlalchemy.plugins import AsyncSessionConfig, SQLAlchemyAsyncConfig, SQLAlchemyInitPlugin
+from litestar.contrib.sqlalchemy.repository import (
+    ModelT,
+    SQLAlchemyAsyncRepository,
+)
+from litestar.di import Provide
+from litestar.openapi import OpenAPIConfig
+from litestar.params import Parameter
+from litestar.repository.filters import LimitOffset
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+
+def provide_limit_offset_pagination(
+    current_page: int = Parameter(ge=1, query="currentPage", default=1, required=False),
+    page_size: int = Parameter(
+        query="pageSize",
+        ge=1,
+        default=10,
+        required=False,
+    ),
+) -> LimitOffset:
+    """Add offset/limit pagination.
+
+    Return type consumed by `Repository.apply_limit_offset_pagination()`.
+
+    Parameters
+    ----------
+    current_page : int
+        LIMIT to apply to select.
+    page_size : int
+        OFFSET to apply to select.
+    """
+    return LimitOffset(page_size, page_size * (current_page - 1))
+
+
+class BaseModel(_BaseModel):
+    """Extend Pydantic's BaseModel to enable ORM mode"""
+
+    model_config = {"from_attributes": True}
+
+
+# we are going to add a simple "slug" to our model that is a URL safe surrogate key to
+# our database record.
+@declarative_mixin
+class SlugKey:
+    """Slug unique Field Model Mixin."""
+
+    __abstract__ = True
+    slug: Mapped[str] = mapped_column(String(length=100), nullable=False, unique=True, sort_order=-9)
+
+
+# this class can be re-used with any model that has the `SlugKey` Mixin
+class SQLAlchemyAsyncSlugRepository(SQLAlchemyAsyncRepository[ModelT]):
+    """Extends the repository to include slug model features."""
+
+    async def get_available_slug(self, value_to_slugify: str, **kwargs: Any, ) -> str:
+        """Get a unique slug for the supplied value.
+
+        If the value is found to exist, a random 4 digit character is appended to the end.
+        There may be a better way to do this, but I wanted to limit the number of
+        additional database calls.
+
+        Args:
+            value_to_slugify (str): A string that should be converted to a unique slug.
+            **kwargs: stuff
+
+        Returns:
+            str: a unique slug for the supplied value. This is safe for URLs and other
+            unique identifiers.
+        """
+        slug = self._slugify(value_to_slugify)
+        if await self._is_slug_unique(slug):
+            return slug
+        # generate a random 4 digit alphanumeric string to make the slug unique and
+        # avoid another DB lookup.
+        random_string = "".join(random.choices(string.ascii_lowercase + string.digits, k=4))
+        return f"{slug}-{random_string}"
+
+    @staticmethod
+    def _slugify(value: str) -> str:
+        """slugify.
+
+        Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+        dashes to single dashes. Remove characters that aren't alphanumerics,
+        underscores, or hyphens. Convert to lowercase. Also strip leading and
+        trailing whitespace, dashes, and underscores.
+
+        Args:
+            value (str): the string to slugify
+
+        Returns:
+            str: a slugified string of the value parameter
+        """
+        value = unicodedata.normalize("NFKD", value).encode("ascii", "ignore").decode("ascii")
+        value = re.sub(r"[^\w\s-]", "", value.lower())
+        return re.sub(r"[-\s]+", "-", value).strip("-_")
+
+    async def _is_slug_unique(self, slug: str, **kwargs: Any, ) -> bool:
+        return await self.get_one_or_none(slug=slug) is None
