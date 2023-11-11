4f8fcee62f52b84dd81cefc54af5bbe723699d1a
Pete Letkeman <pete@letkeman.ca>
2023-11-10 22:20:58 -0500

    added attribute class

--- meta_data.py
+++ meta_data.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Optional
+from typing import TYPE_CHECKING, Any, Optional, List
 from uuid import UUID
 
 from litestar import HttpMethod
 from litestar import route
 from litestar.pagination import OffsetPagination
 from pydantic import TypeAdapter
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from litestar import Controller
 from litestar import get, post, delete
 from litestar.contrib.sqlalchemy.base import UUIDAuditBase
@@ -25,9 +26,30 @@ from shared import SlugKey, SQLAlchemyAsyncSlugRepository, BaseModel
 # additional columns: `created` and `updated`. `created` is a timestamp of when the
 # record created, and `updated` is the last time the record was modified.
 class MetaDataTag(UUIDAuditBase, SlugKey):
-    sortOrder: Mapped[int | None] = 0
+    """
+    <dc:rights>Public domain in the USA.</dc:rights>
+    <meta property="role" refines="#author_0" scheme="marc:relators">aut</meta>
+    MetaDataTag can have zero or more attributes
+    """
+    __tablename__ = 'meta_data_tag'
+
+    sort_order: Mapped[int | None] = 0
     name: Mapped[str]
+    tag: Mapped[str]
+    is_empty_tag: Mapped[int | None] = 0
     description: Mapped[str | None] = None
+
+    attribute_id: Mapped[Optional[int]] = mapped_column(ForeignKey("attribute.id"))
+    item_tag: Mapped[Optional["Attribute"]] = relationship(back_populates="attributes")
+
+
+class Attribute(UUIDAuditBase):
+    __tablename__ = "attribute"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    sort_order: Mapped[int | None] = 0
+    attributes: Mapped[List["MetaDataTag"]] = relationship(back_populates="item_tag")
 
 
 class MetaDataTagRepository(SQLAlchemyAsyncSlugRepository[MetaDataTag]):
@@ -38,21 +60,27 @@ class MetaDataTagRepository(SQLAlchemyAsyncSlugRepository[MetaDataTag]):
 
 class MetaDataTagDTO(BaseModel):
     id: UUID | None
-    sortOrder: Optional[int] = 0
+    sort_order: Optional[int] = 0
     slug: str
     name: str
+    tag: str
+    is_empty_tag: Optional[int] = 0
     description: Optional[str] = None
 
 
 class MetaDataTagCreate(BaseModel):
     name: str
-    sortOrder: Optional[int] = 0
+    tag: str
+    is_empty_tag: Optional[int] = 0
+    sort_order: Optional[int] = 0
     description: Optional[str] = None
 
 
 class MetaDataTagUpdate(BaseModel):
-    sortOrder: Optional[int] = 0
+    sort_order: Optional[int] = 0
     name: str
+    tag: str
+    is_empty_tag: Optional[int] = 0
     description: Optional[str] = None
 
 
