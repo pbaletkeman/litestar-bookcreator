cdc528dc4b7687488ed776c7fa5dfe714b432009
Pete Letkeman <pete@letkeman.ca>
2023-11-11 17:36:24 -0500

    began nested endpoints

--- meta_data.py
+++ meta_data.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
+import uuid
 from typing import TYPE_CHECKING, Any, Optional, List
 from uuid import UUID
 
+from advanced_alchemy import SQLAlchemyAsyncRepository
 from litestar import HttpMethod
 from litestar import route
 from litestar.pagination import OffsetPagination
 from pydantic import TypeAdapter
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, select
 from sqlalchemy.types import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from litestar import Controller
@@ -16,6 +18,7 @@ from litestar.contrib.sqlalchemy.base import UUIDAuditBase
 from litestar.di import Provide
 from litestar.params import Parameter
 from litestar.repository.filters import LimitOffset
+from sqlalchemy import lambda_stmt
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
@@ -46,22 +49,6 @@ class MetaDataTag(UUIDAuditBase, SlugKey):
         super().__init__(**kw)
         if self.is_empty_tag is None:
             self.is_empty_tag = False
-        if self.sort_order is None:
-            self.sort_order = 0
-
-
-class Attribute(UUIDAuditBase):
-    __tablename__ = "attribute"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    sort_order: Mapped[int | None] = mapped_column(nullable=False, default=0)  # = mapped_column("sortOrder")
-    name: Mapped[str] = mapped_column(String(length=30), nullable=False)
-
-    meta_data_tag_id: Mapped[UUID] = mapped_column(ForeignKey("meta_data_tag.id"))
-    item_tag: Mapped["MetaDataTag"] = relationship(back_populates="attributes", lazy="selectin")
-
-    def __init__(self, **kw: Any):
-        super().__init__(**kw)
         if self.sort_order is None:
             self.sort_order = 0
 
@@ -106,14 +93,85 @@ async def provide_meta_data_tag_repo(db_session: AsyncSession) -> MetaDataTagRep
     return MetaDataTagRepository(session=db_session)
 
 
+class Attribute(UUIDAuditBase):
+    __tablename__ = "attribute"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    sort_order: Mapped[int | None] = mapped_column(nullable=False, default=0)  # = mapped_column("sortOrder")
+    name: Mapped[str] = mapped_column(String(length=30), nullable=False)
+
+    meta_data_tag_id: Mapped[UUID] = mapped_column(ForeignKey("meta_data_tag.id"))
+    item_tag: Mapped["MetaDataTag"] = relationship(back_populates="attributes", lazy="selectin")
+
+    def __init__(self, **kw: Any):
+        super().__init__(**kw)
+        if self.sort_order is None:
+            self.sort_order = 0
+
+
+class AttributeRepository(SQLAlchemyAsyncRepository[Attribute]):
+    """Blog Post repository."""
+    model_type = Attribute
+
+
+class AttributeDTO(BaseModel):
+    id: int | None
+    sort_order: Optional[int] = 0
+    name: str
+    description: Optional[str] = None
+    meta_data_tag_id: UUID
+
+
+class AttributeCreate(BaseModel):
+    name: str
+    sort_order: Optional[int] = 0
+    description: Optional[str] = None
+    meta_data_tag_id: UUID
+
+
+class AttributeUpdate(BaseModel):
+    name: str
+    sort_order: Optional[int] = 0
+    description: Optional[str] = None
+    meta_data_tag_id: UUID
+
+
+# we can optionally override the default `select` used for the repository to pass in
+# specific SQL options such as join details
+async def provide_attribute_repo(db_session: AsyncSession) -> AttributeRepository:
+    """This provides a simple example demonstrating how to override the join options
+    for the repository."""
+    return AttributeRepository(session=db_session)
+
+
 class MetaDataTagController(Controller):
     path = '/meta-data-tag'
     controller_tag = ['Meta Data']
 
-    dependencies = {"meta_data_tag_repo": Provide(provide_meta_data_tag_repo)}
+    dependencies = {"meta_data_tag_repo": Provide(provide_meta_data_tag_repo),
+                    "attribute_repo": Provide(provide_attribute_repo)}
+
+    @get(path='/{meta_data_tag_id:str}/attributes/', tags=controller_tag)
+    async def list_attribute_items(
+            self,
+            meta_data_tag_id: str,
+            attribute_repo: AttributeRepository,
+            limit_offset: LimitOffset,
+    ) -> OffsetPagination[AttributeDTO]:
+        """List items."""
+        stmt = lambda_stmt(lambda: select(Attribute))
+        stmt += lambda s: s.where(Attribute.meta_data_tag_id == meta_data_tag_id)
+        results, total = await attribute_repo.list_and_count(limit_offset, statement=stmt)
+        type_adapter = TypeAdapter(list[AttributeDTO])
+        return OffsetPagination[AttributeDTO](
+            items=type_adapter.validate_python(results),
+            total=total,
+            limit=limit_offset.limit,
+            offset=limit_offset.offset,
+        )
 
     @get(path='/', tags=controller_tag)
-    async def list_items(
+    async def list_meta_data_items(
             self,
             meta_data_tag_repo: MetaDataTagRepository,
             limit_offset: LimitOffset,
@@ -129,13 +187,13 @@ class MetaDataTagController(Controller):
         )
 
     @get(path="/{item_slug:str}", tags=controller_tag)
-    async def get_item_details(self, item_slug: str, meta_data_tag_repo: MetaDataTagRepository, ) -> MetaDataTagDTO:
+    async def get_meta_data_details(self, item_slug: str, meta_data_tag_repo: MetaDataTagRepository, ) -> MetaDataTagDTO:
         """Interact with SQLAlchemy engine and session."""
         obj = await meta_data_tag_repo.get_one(slug=item_slug)
         return MetaDataTagDTO.model_validate(obj)
 
     @post(path="/", tags=controller_tag)
-    async def create_item(self, meta_data_tag_repo: MetaDataTagRepository, data: MetaDataTagCreate, ) -> MetaDataTagDTO:
+    async def create_meta_data_item(self, meta_data_tag_repo: MetaDataTagRepository, data: MetaDataTagCreate, ) -> MetaDataTagDTO:
         """Create a new meta_data tag."""
         _data = data.model_dump(exclude_unset=True, by_alias=False, exclude_none=True)
         _data["slug"] = await meta_data_tag_repo.get_available_slug(_data["name"])
@@ -144,7 +202,7 @@ class MetaDataTagController(Controller):
         return MetaDataTagDTO.model_validate(obj)
 
     @route(path="/{id:uuid}", http_method=[HttpMethod.PUT, HttpMethod.PATCH], tags=controller_tag)
-    async def update_item(
+    async def update_meta_data_item(
             self,
             meta_data_tag_repo: MetaDataTagRepository,
             data: MetaDataTagUpdate,
@@ -158,7 +216,7 @@ class MetaDataTagController(Controller):
         return MetaDataTagUpdate.model_validate(obj)
 
     @delete(path="/{id:uuid}", tags=controller_tag)
-    async def delete_item(
+    async def delete_meta_data_item(
             self,
             meta_data_tag_repo: MetaDataTagRepository,
             id: UUID = Parameter(title="MetaData.py ID", description="The meta_data to delete.", ),
